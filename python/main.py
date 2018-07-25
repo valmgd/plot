aec39d92e9a052f957195a2cd45c628c93905342
valmdg <valentin.magda@nextflow-software.com>
2018-07-25 14:29:26 +0200

    Add Particles class & plot FTS force

--- python/main.py
+++ python/main.py
@@ -10,88 +10,49 @@ from plot import *
 # -----------------------------------------------------------------------------------------------------------
 # données
 # -----------------------------------------------------------------------------------------------------------
-x      = np.loadtxt('../out-sph/sans-ts/x.dat')
-y      = np.loadtxt('../out-sph/sans-ts/y.dat')
-mvx    = np.loadtxt('../out-sph/sans-ts/mvx.dat')
-mvy    = np.loadtxt('../out-sph/sans-ts/mvy.dat')
-P      = np.loadtxt('../out-sph/sans-ts/P.dat')
-vx     = np.loadtxt('../out-sph/sans-ts/vx.dat')
-vy     = np.loadtxt('../out-sph/sans-ts/vy.dat')
-
-ts_x   = np.loadtxt('../out-sph/avec-ts/x.dat')
-ts_y   = np.loadtxt('../out-sph/avec-ts/y.dat')
-ts_mvx = np.loadtxt('../out-sph/avec-ts/mvx.dat')
-ts_mvy = np.loadtxt('../out-sph/avec-ts/mvy.dat')
-ts_P   = np.loadtxt('../out-sph/avec-ts/P.dat')
-ts_vx  = np.loadtxt('../out-sph/avec-ts/vx.dat')
-ts_vy  = np.loadtxt('../out-sph/avec-ts/vy.dat')
-FTSx   = np.loadtxt('../out-sph/avec-ts/FTSx.dat')
-FTSy   = np.loadtxt('../out-sph/avec-ts/FTSy.dat')
-ts_w   = np.loadtxt('../out-sph/avec-ts/w.dat')
-
-n = len(x)
-ts_n = len(ts_x)
+part = Particles('avec-ts')
 
 
 
 # -----------------------------------------------------------------------------------------------------------
 # calculs somme sur quartiers
 # -----------------------------------------------------------------------------------------------------------
-print('nombre de particules : ', n, ts_n)
-
-somme_x, somme_y = quarter(x, y, mvx, mvy)
-print('\nSomme Dmv/Dt sur un quartier sans TS :')
+somme_x, somme_y = quarter(part)
+print('\nSomme Dmv/Dt sur un quartier :')
 print(somme_x, somme_y)
-
-somme_x, somme_y = quarter(ts_x, ts_y, ts_mvx, ts_mvy)
-print('\nSomme Dmv/Dt sur un quartier avec TS :')
-print(somme_x, somme_y)
-
-# print('\nDifférence entre Dmv/Dt avec TS et sans TS en norme infinie :')
-# print(max(abs(mvx - ts_mvx)), max(abs(mvy - ts_mvy)))
 
 # quantité de mouvement max
-qmvMax = 'Dmv max en norme euclidienne : ' + str(max(np.sqrt(ts_mvx**2 + ts_mvy**2)))
+qmvMax = 'Dmv max en norme euclidienne : ' + str(max(np.sqrt(part.mvx**2 + part.mvy**2)))
 print('\n', qmvMax)
 
 # intervalle de pression
-ip = 'Intervalle de pression : ' + '[' + str(min(ts_P)) + ',' + str(max(ts_P)) + ']'
+ip = 'Intervalle de pression : ' + '[' + str(min(part.P)) + ',' + str(max(part.P)) + ']'
 print('\n', ip)
 
 # vitesse max
-vMax = 'Vitesse max en norme euclidienne : ' + str(max(np.sqrt(ts_vx**2 + ts_vy**2)))
+vMax = 'Vitesse max en norme euclidienne : ' + str(max(np.sqrt(part.vx**2 + part.vy**2)))
 print('\n', vMax)
 
 # volume total
-wTot = 'Volume total :' + str(sum(ts_w))
+wTot = 'Volume total :' + str(sum(part.w))
 print('\n', wTot)
 
 
 
 # -----------------------------------------------------------------------------------------------------------
-# sans ts
-# -----------------------------------------------------------------------------------------------------------
-# fig1, ax1 = plot_bulle(x, y, mvx, mvy, P, 'Quantité de mvt sans TS')
-
-
-
-# -----------------------------------------------------------------------------------------------------------
-# avec ts
+# Quantité de mouvement
 # -----------------------------------------------------------------------------------------------------------
 titre1 = 'Quantité de mvt avec TS, t=1s\n initialisation bulle avec pression de Laplace'
 titre2 = 'Quantité de mvt avec TS, t=240s\n initialisation carré avec pression quelconque'
-fig2, ax2 = plot_bulle(ts_x, ts_y, ts_mvx, ts_mvy, ts_P, titre2)
-ax2.text(0.005, -0.011, s=qmvMax)
-ax2.text(0.005, -0.012, s=ip)
-ax2.text(0.005, -0.013, s=vMax)
-ax2.text(0.005, -0.014, s=wTot)
+fig, ax = part.plot_mvt_quantity(titre2)
+ax.text(0.005, -0.011, s=qmvMax)
+ax.text(0.005, -0.012, s=ip)
+ax.text(0.005, -0.013, s=vMax)
+ax.text(0.005, -0.014, s=wTot)
+
+part.plot_ts_forces()
 
 
 
-# -----------------------------------------------------------------------------------------------------------
-# gradient de pression et tension de surface
-# -----------------------------------------------------------------------------------------------------------
-wGRPx = ts_w * FTSx - ts_mvx
-wGRPy = ts_w * FTSy - ts_mvy
-
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 plt.show()
--- python/plot.py
+++ python/plot.py
@@ -2,46 +2,113 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Particles :
-    def __init__(self) :
-        self.x = 0
+    # -------------------------------------------------------------------------------------------------------
+    # constructeur
+    # -------------------------------------------------------------------------------------------------------
+    def __init__(self, directory) :
+        # coordonnées
+        self.x    = np.loadtxt('../out-sph/' + directory + '/x.dat')
+        self.y    = np.loadtxt('../out-sph/' + directory + '/y.dat')
+
+        # quantité de mvt
+        self.mvx  = np.loadtxt('../out-sph/' + directory + '/mvx.dat')
+        self.mvy  = np.loadtxt('../out-sph/' + directory + '/mvy.dat')
+
+        # pression
+        self.P    = np.loadtxt('../out-sph/' + directory + '/P.dat')
+
+        # vitesse
+        self.vx   = np.loadtxt('../out-sph/' + directory + '/vx.dat')
+        self.vy   = np.loadtxt('../out-sph/' + directory + '/vy.dat')
+
+        # Tension de surface
+        self.FTSx = np.loadtxt('../out-sph/' + directory + '/FTSx.dat')
+        self.FTSy = np.loadtxt('../out-sph/' + directory + '/FTSy.dat')
+
+        # volume
+        self.w    = np.loadtxt('../out-sph/' + directory + '/w.dat')
+
+        # gradient de pression
+        self.GRPx = (self.w * self.FTSx - self.mvx) / self.w
+        self.GRPy = (self.w * self.FTSy - self.mvy) / self.w
+    #}
+
+    # -------------------------------------------------------------------------------------------------------
+    # plot champs de vecteur : quantité de mvt
+    # -------------------------------------------------------------------------------------------------------
+    def plot_mvt_quantity(self, title) :
+        fig, ax = plt.subplots()
+        im = ax.scatter(self.x, self.y, s=20, c=self.P, cmap='jet', label='Particules')
+        cax = fig.colorbar(im, ax=ax)
+        cax.set_label('Pressure [Pa]')
+
+        ax.quiver(self.x, self.y, self.mvx, self.mvy, angles='xy', scale=0.05, label='Quantité mvt', color='black')
+
+        ax.set_xlabel('$x$ [m]')
+        ax.set_ylabel('$y$ [m]')
+        ax.set_aspect('equal', 'datalim')
+        ax.set_facecolor('lavender')
+        ax.set_title(title)
+
+        ax.legend(loc='best', fontsize='x-large', fancybox=True, framealpha=0.5)
+        ax.grid(color='gray', linestyle='--')
+
+        fig.tight_layout()
+
+
+        return(fig, ax)
+    #}
+
+    # -------------------------------------------------------------------------------------------------------
+    # plot 2 champs de vecteurs : le gradient de pression et la tension de surface
+    # -------------------------------------------------------------------------------------------------------
+    def plot_ts_forces(self) :
+        sc = 0.01
+
+        fig, ax = plt.subplots(ncols=2)
+
+        ax[0].scatter(self.x, self.y, s=20, label='Particules')
+        ax[0].quiver(self.x, self.y, self.GRPx, self.GRPy, angles='xy', scale=sc, label='GRP', color='black')
+        ax[0].set_xlabel('$x$ [m]')
+        ax[0].set_ylabel('$y$ [m]')
+        ax[0].set_aspect('equal', 'datalim')
+        ax[0].set_facecolor('lavender')
+        ax[0].set_title('Gradient de pression')
+        ax[0].set_xlim((-0.02, 0.02))
+        ax[0].set_ylim((-0.02, 0.02))
+
+        ax[0].legend(loc='best', fontsize='x-large', fancybox=True, framealpha=0.5)
+        ax[0].grid(color='gray', linestyle='--')
+
+        ax[1].scatter(self.x, self.y, s=20, label='Particules')
+        ax[1].quiver(self.x, self.y, self.FTSx, self.FTSy, angles='xy', scale=sc, label='FTS', color='black')
+        ax[1].set_xlabel('$x$ [m]')
+        ax[1].set_ylabel('$y$ [m]')
+        ax[1].set_aspect('equal', 'datalim')
+        ax[1].set_facecolor('lavender')
+        ax[1].set_title('Tension de surface')
+        ax[1].set_xlim((-0.02, 0.02))
+        ax[1].set_ylim((-0.02, 0.02))
+
+        ax[1].legend(loc='best', fontsize='x-large', fancybox=True, framealpha=0.5)
+        ax[1].grid(color='gray', linestyle='--')
+
+
+        fig.tight_layout()
+
+        return(fig, ax)
     #}
 #}
 
-def plot_bulle(x, y, mvx, mvy, P, title) :
-    # -------------------------------------------------------------------------------------------------------
-    # tracés
-    # -------------------------------------------------------------------------------------------------------
-    fig, ax = plt.subplots()
-    im = ax.scatter(x, y, s=20, c=P, cmap='jet', label='Particules')
-    cax = fig.colorbar(im, ax=ax)
-    cax.set_label('Pressure [Pa]')
 
-    ax.quiver(x, y, mvx, mvy, angles='xy', scale=0.05, label='Quantité mvt', color='black')
 
-    # -------------------------------------------------------------------------------------------------------
-    # affichage
-    # -------------------------------------------------------------------------------------------------------
-    ax.set_xlabel('$x$ [m]')
-    ax.set_ylabel('$y$ [m]')
-    ax.set_aspect('equal', 'datalim')
-    ax.set_facecolor('lavender')
-    ax.set_title(title)
-
-    ax.legend(loc='best', fontsize='x-large', fancybox=True, framealpha=0.5)
-    ax.grid(color='gray', linestyle='--')
-
-    fig.tight_layout()
-
-    return(fig, ax)
-#}
-
-def quarter(x, y, mvx, mvy) :
-    n = len(x)
+def quarter(part) :
+    n = len(part.x)
     somme_x = somme_y = 0.
     for i in range(0, n) :
-        if x[i] >= 0 and y[i] >= 0 :
-            somme_x += mvx[i]
-            somme_y += mvy[i]
+        if part.x[i] >= 0 and part.y[i] >= 0 :
+            somme_x += part.mvx[i]
+            somme_y += part.mvy[i]
         #}
     #}
     return(somme_x, somme_y)
